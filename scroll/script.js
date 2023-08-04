416187ce880c65f3a50ad6f9e261d855526e3bbf
AzureAD\엄지민 <eommji@bellins.net>
2023-08-04 17:25:33 +0900

    scroll 작업 중

--- scroll/script.js
+++ scroll/script.js
@@ -1,40 +1,43 @@
-// const swiper = new Swiper('.swiper', {
-//   slidesPerView: 'auto',
-//   spaceBetween: 10,
-// });
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+});
 
 // let eventTrigger2 = false;
 
-// function tabCenter(target) {
-//   var tabSwiper = $('.swiper'),
-//     tabWrapper = target.closest('.swiper-wrapper'),
-//     tabItem = tabWrapper.find('.swiper-slide'),
-//     tabSwiperWidth = tabSwiper.width(),
-//     targetPos = target.position(),
-//     tabSwiperHarf = tabSwiper.width() / 2;
-//   var pos;
-//   var listWidth = 0;
-//   tabItem.each(function () {
-//     listWidth += $(this).outerWidth() + 12;
-//   });
-//   var selectTargetPos = targetPos.left + target.outerWidth() / 2;
-//   if (eventTrigger2) return;
-//   if (selectTargetPos <= tabSwiperHarf) {
-//     pos = 0;
-//   } else if ((listWidth - selectTargetPos) <= tabSwiperHarf) {
-//     pos = listWidth - tabSwiper.width();
-//   } else {
-//     pos = selectTargetPos - tabSwiperHarf;
-//   }
-//   if (listWidth > tabSwiperWidth) {
-//     setTimeout(function () {
-//       tabWrapper.css({
-//         'transform': 'translate3d(' + (pos * -1) + 'px, 0, 0)',
-//         'transition-duration': '450ms'
-//       })
-//     }, 0);
-//   }
-// }
+function tabCenter(target) {
+  var tabSwiper = $('.swiper'),
+    tabWrapper = target.closest('.swiper-wrapper'),
+    tabItem = tabWrapper.find('.swiper-slide'),
+    tabSwiperWidth = tabSwiper.width(),
+    targetPos = target.position(),
+    tabSwiperHarf = tabSwiper.width() / 2;
+  var pos;
+  var listWidth = 0;
+
+  tabItem.each(function () {
+    listWidth += $(this).outerWidth() + 12;
+  }); // swiper-slide들의 총 길이
+
+  var selectTargetPos = targetPos.left + target.outerWidth() / 2; // target 위치
+
+  if (eventTrigger2) return;
+  if (selectTargetPos <= tabSwiperHarf) {
+    pos = 0;
+  } else if ((listWidth - selectTargetPos) <= tabSwiperHarf) {
+    pos = listWidth - tabSwiper.width();
+  } else {
+    pos = selectTargetPos - tabSwiperHarf;
+  }
+  if (listWidth > tabSwiperWidth) {
+    setTimeout(function () {
+      tabWrapper.css({
+        'transform': 'translate3d(' + (pos * -1) + 'px, 0, 0)',
+        'transition-duration': '450ms'
+      })
+    }, 0);
+  }
+}
 
 // const $ctrlDiv = $(".content");
 // let eventTrigger = false;
@@ -71,27 +74,74 @@ const on_CN = 'on';
 const container = document.querySelector('.container');
 const content = document.querySelectorAll('.content');
 
-let pending
+let sum = 0;
+slides.forEach(e => sum += e.offsetWidth);
 
-wrapper.addEventListener('click', e => {
-  const targetSlide = e.target.closest('.swiper-slide');
-  if (!targetSlide) return;
-  slides.forEach(e => e.classList.remove(on_CN));
-  targetSlide.classList.add(on_CN);
+const slideMargin = parseInt(document.querySelector('.swiper-slide').style.marginRight);
+const slidesWidth = sum + (slideMargin * (slides.length - 1));
 
-  const slideIndex = [...slides].indexOf(targetSlide);
-  const contentTarget = [...content][slideIndex];
-  const contentTargetOffsetTop = contentTarget.offsetTop;
 
-  window.scrollTo({
-    top: contentTargetOffsetTop,
-    behavior: 'smooth'
+const bodyWidth = document.querySelector('body').offsetWidth;
+const bodyWidthHalf = document.querySelector('body').offsetWidth / 2; // 252
+
+
+
+const handleCenter = (targetSlide) => {
+  const targetSlidePos = targetSlide.offsetLeft + (targetSlide.offsetWidth / 2);
+  let xPos = 0;
+
+  if (targetSlidePos <= bodyWidthHalf) {
+    // 앞쪽에 위치하고 있어서 앞에 붙음
+    // return;을 해도 되는지 아님 xPos = 0;을 해야 하는지
+  } else if ((slidesWidth - targetSlidePos) <= bodyWidthHalf) {
+    // 뒤쪽에 위치하고 있어서 뒤에 붙음
+    xPos = -(slidesWidth - bodyWidth);
+    // wrapper.style.transform = `translate3d(${-(slidesWidth - bodyWidth)}px, 0px, 0px)`;
+  } else {
+    // 이동함
+    xPos = -(targetSlidePos - bodyWidthHalf);
+    // wrapper.style.transform = `translate3d(${-(targetSlidePos - bodyWidthHalf)}px, 0px, 0px)`;
+  }
+
+  // wrapper.style.transform = `translate3d(0px, 0px, 0px)`;
+}
+
+
+
+
+// let isPending = false;
+
+const menuClick = () => {
+  wrapper.addEventListener('click', e => {
+    const targetSlide = e.target.closest('.swiper-slide');
+
+    handleCenter(targetSlide);
+    // isPending = true;
+
+    if (!targetSlide) return;
+    slides.forEach(e => e.classList.remove(on_CN));
+    targetSlide.classList.add(on_CN);
+
+    const slideIndex = [...slides].indexOf(targetSlide);
+    const contentTarget = [...content][slideIndex];
+    const contentTargetOffsetTop = contentTarget.offsetTop;
+
+    window.scrollTo({
+      top: contentTargetOffsetTop + 1,
+      behavior: 'smooth'
+    });
+
+    // setTimeout(() => {
+    //   isPending = false;
+    // }, 1000);
   });
-  console.dir();
-});
+};
 
 const contentScroll = () => {
   window.addEventListener('scroll', () => {
+
+    // if (isPending === true) return;
+
     const windowY = this.scrollY;
     const targetIndex = [...document.querySelectorAll('.content')].findIndex(e => {
       return windowY >= e.offsetTop && windowY <= e.offsetTop + e.offsetHeight;
@@ -110,4 +160,5 @@ const contentScroll = () => {
   });
 };
 
+menuClick();
 contentScroll();
